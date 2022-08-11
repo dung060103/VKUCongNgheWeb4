a4d8383b6f3d1888ae22b4e359c07252f83d779a
unknown <ilovethubum@gmail.com>
2022-08-11 15:53:03 +0700

    first commit

---
+++ a.php
@@ -0,0 +1,3 @@
+<?php
+echo "Chao mung cac ban den voi lap trinh web"
+?>
---
+++ vidu.php
@@ -0,0 +1,9 @@
+<?php
+include("a.php");
+echo"</br>";
+echo"hom nay troi nhe len cao";
+echo"</br>";
+echo"toi buon khong hieu vi sao toi buon";
+echo"</br>";
+include("a.php");
+?>
